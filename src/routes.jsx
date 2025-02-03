ea51f36f08ab790269b7758b9a1d4254140841a1
Angela-G9 <angelagathoni9568@gmail.com>
2025-02-03 21:36:34 +0300

    lab done

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,25 @@
+// src/routes.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About'; // Only one About import
+import Directors from './pages/Directors';
+import Actors from './pages/Actors';
+import Movie from './pages/Movie';
+import NavBar from './components/NavBar';
 
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/directors" element={<Directors />} />
+        <Route path="/actors" element={<Actors />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </Router>
+  );
+}
 
-const routes = [
-   
-  ];
-
-export default routes;
+export default App;
